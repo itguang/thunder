9969ae82b3cd88bc4cb0aa792f9c283449de92e5
guang <guang@xingren.com>
2020-12-20 15:55:36 +0800

    Initial commit

---
+++ src/main/java/com/pj/thunder/ThunderApplication.java
@@ -0,0 +1,13 @@
+package com.pj.thunder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThunderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThunderApplication.class, args);
+    }
+
+}
